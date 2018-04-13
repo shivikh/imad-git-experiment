386a07e70d3ab61780503bd87b66fc8e630b5a24
shivani <skhandelwal58821@imad-feb-18-ssh.c.hasura-imad.internal>
2018-04-13 07:58:39 +0000

    halfway commit with new feature

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
+function aBigNewFeatures(){
+console.log('writing lot of code');
+console.log('half new features is done');
+}
+
+
 console.log('hello worls');
 function something()
 {
  console.log('some function');
 }
 something();
+aBigNewFeatures();
